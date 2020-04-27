36d4bba6a91ea26eaa276591acbedda521cf9c25
Qianying Wang <w.qianying@outlook.com>
2020-04-27 20:43:15 +0100

    init args and setting

---
+++ bert/arg_parser.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 27 11:25:41 2020
+
+@author: qwang
+"""
+
+import argparse
+import json
+import os
+
+USER = os.getenv('USER')
+
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
+def get_args():
+    """
+    Returns a namedtuple with arguments extracted from the command line.
+    :return: A namedtuple with arguments
+    """
+    parser = argparse.ArgumentParser(description='RoB training and inference helper script')
+
+    
+    # Experiments
+    parser.add_argument('--seed', nargs="?", type=int, default=1234, help='Seed for random number generator')
+    parser.add_argument('--batch_size', nargs="?", type=int, default=16, help='Batch size')
+    parser.add_argument('--num_epochs', nargs="?", type=int, default=4, help='Number of epochs')    
+    parser.add_argument('--train_ratio', nargs="?", type=float, default=0.8, help='Ratio of training set')
+    parser.add_argument('--val_ratio', nargs="?", type=float, default=0.1, help='Ratio of validation set')
+
+    
+    parser.add_argument('--info_file', nargs="?", type=str, default="/media/mynewdrive/rob/data/rob_info_a.pkl", help='Path of info pickle file')
+    parser.add_argument('--pkl_dir', nargs="?", type=str, default="/media/mynewdrive/rob/data/rob_str", help='Directory of pickle files')
+    parser.add_argument('--args_json_path', nargs="?", type=str, default=None, help='Path of argument json file')
+    parser.add_argument('--exp_dir', nargs="?", type=str, default="/home/qwang/rob/bert", help='Folder of the experiment')
+    parser.add_argument('--save_model', nargs="?", type=str2bool, default=False, help='Save model.pth.tar with best loss')
+    
+       
+    # RoB item
+    parser.add_argument('--rob_item', nargs="?", type=str, default="RandomizationTreatmentControl", 
+                        choices=['RandomizationTreatmentControl',
+                                 'BlindedOutcomeAssessment',
+                                 'SampleSizeCalculation',
+                                 'AnimalExclusions',
+                                 'AllocationConcealment',
+                                 'AnimalWelfareRegulations',
+                                 'ConflictsOfInterest'], 
+                        help='Risk of bias item')
+
+    # Model
+    parser.add_argument('--net_type', nargs="?", type=str, default='bert_linear', 
+                        choices=['bert_linear', 'bert_lstm', 'bert_transformer'], 
+                        help="Different network models")
+    
+    parser.add_argument('--weight_balance', nargs="?", type=str2bool, default=True, help='Assign class weights for imbalanced data')
+    parser.add_argument('--threshold', nargs="?", type=float, default=0.5, help='Threshold for positive class value')
+    
+    parser.add_argument('--max_chunk_len', nargs="?", type=int, default=512, help='Max context window size for bert')
+    parser.add_argument('--cut_head_ratio', nargs="?", type=float, default=0.1, help='Ratio of tokens cut from head')
+    parser.add_argument('--cut_tail_ratio', nargs="?", type=float, default=0.1, help='Ratio of tokens cut from tail')
+    
+
+    # Transformer encoder
+    parser.add_argument('--num_heads', nargs="?", type=int, default=2, help='Number of heads in the multi-head attention module')
+    parser.add_argument('--ff_dim', nargs="?", type=int, default=10, help='Dimension of the feedforward network model')
+    parser.add_argument('--num_enc_layers', nargs="?", type=int, default=2, help='Number of sub-encoder-layers in the encoder')
+    
+    args = parser.parse_args()
+    
+    if args.args_json_path is None:
+        arg_str = [(str(key), str(value)) for (key, value) in vars(args).items()]
+        print(arg_str)
+    else:
+        args = extract_args_from_json(json_file_path=args.args_json_path, existing_args_dict=args)   
+    
+    return args
+
+
+class AttributeAccessibleDict(object):
+    def __init__(self, adict):
+        self.__dict__.update(adict)
+
+
+def extract_args_from_json(json_file_path, existing_args_dict=None):
+
+    summary_filename = json_file_path
+    with open(summary_filename) as fin:
+        args_dict = json.load(fp=fin)
+
+    for key, value in vars(existing_args_dict).items():
+        if key not in args_dict:
+            args_dict[key] = value
+
+    args_dict = AttributeAccessibleDict(args_dict)
+
+    return args_dict
---
+++ bert/model.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 27 14:56:39 2020
+
+@author: qwang
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from transformers.modeling_bert import BertPreTrainedModel, BertConfig, BertModel
+
+
+
+#%%
+class BertLinear(BertPreTrainedModel):
+
+    def __init__(self, bert_config: BertConfig):
+        super().__init__(bert_config)
+        
+        self.bert = BertModel(bert_config)
+        
+        self.dropout = nn.Dropout(bert_config.hidden_dropout_prob)
+        self.fc = nn.Linear(bert_config.hidden_size * bert_config.n_chunks, bert_config.num_labels)
+        # self.init_weights()
+    
+    def forward(self, doc):
+        """
+        Input:
+            doc: [batch_size, num_chunks, 3, max_chunk_len]            
+        Returns:
+            out: [batch_size, output_dim]       
+            
+        """    
+        batch_size = doc.shape[0]        
+        # num_chunks = doc.shape[1]
+        
+        # Actual n_chunks vary in different batches, but dimension at FC layer needs to be same for each batch
+        # so we set "n_chunks" same over all batches. Hence, last part of some docs might be cut off
+        pooled = torch.zeros((batch_size, self.bert.config.n_chunks, self.bert.config.hidden_size), dtype=torch.float)
+        for i in range(batch_size):
+            # Output of BertModel: (last_hidden_state, pooler_output, hidden_states, attentions)
+            # Last layer hidden-state of the first token of the sequence (classification token)
+            pooled[i] = self.bert(input_ids = doc[i,:self.bert.config.n_chunks,0], 
+                                  attention_mask = doc[i,:self.bert.config.n_chunks,1], 
+                                  token_type_ids = doc[i,:self.bert.config.n_chunks,2])[1]
+        
+        
+        dp = self.dropout(pooled)  # [batch_size, n_chunks, bert_hidden_size]
+        dp.view(batch_size, -1)  # [batch_size, n_chunks*bert_hidden_size]
+        out = self.fc(dp)  # [batch_size, num_labels]         
+        out = F.softmax(out, dim=1)  # [batch_size, num_labels]
+             
+        return out
+
+
+#%%
+class BertLSTM(BertPreTrainedModel):
+
+    def __init__(self, bert_config: BertConfig):
+        super().__init__(bert_config)
+        
+        self.bert = BertModel(bert_config)
+        
+        self.dropout = nn.Dropout(bert_config.hidden_dropout_prob)
+                
+        self.lstm = nn.LSTM(input_size = bert_config.hidden_size, hidden_size = bert_config.hidden_size,
+                            num_layers = 1, dropout = 0, 
+                            batch_first = True, bidirectional = False)
+        
+        self.fc = nn.Linear(bert_config.hidden_size, bert_config.num_labels)
+        # self.init_weights()
+    
+    def forward(self, doc):
+        """
+        Input:
+            doc: [batch_size, num_chunks, 3, max_chunk_len]            
+        Returns:
+            out: [batch_size, output_dim]       
+            
+        """    
+        batch_size = doc.shape[0]        
+        num_chunks = doc.shape[1]
+        
+        pooled = torch.zeros((batch_size, num_chunks, self.bert.config.hidden_size), dtype=torch.float)
+        for i in range(batch_size):
+            # Output of BertModel: (last_hidden_state, pooler_output, hidden_states, attentions)
+            # Last layer hidden-state of the first token of the sequence (classification token)
+            pooled[i] = self.bert(input_ids = doc[i,:,0], 
+                                  attention_mask = doc[i,:,1], 
+                                  token_type_ids = doc[i,:,2])[1]
+        
+        
+        dp = self.dropout(pooled)  # [batch_size, num_chunks, bert_hidden_size]
+        # output: [batch_size, num_chunks, n_directions*hidden_size], output features from last layer for each t
+        # h_n: [batch_size, n_layers*n_directions, hidden_size], hidden state for t=seq_len
+        # c_n: [batch_size, n_layers*n_directions, hidden_size], cell state fir t=seq_len
+        output, (h_n, c_n) = self.lstm(dp)
+        
+        h_n = h_n.squeeze(1)  # [batch_size, hidden_size]
+        
+        out = self.fc(h_n)  # [batch_size, num_labels]         
+        out = F.softmax(out, dim=1)  # [batch_size, num_labels]
+        
+        return out
