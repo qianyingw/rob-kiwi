85f048693d4784c8074e422fa244e5289c07a344
Qianying Wang <w.qianying@outlook.com>
2020-05-08 12:02:30 +0100

    typo

--- bert/main.py
+++ bert/main.py
@@ -94,7 +94,7 @@ if args.net_type == 'albert_lstm':
     model = AlbertLSTM(config)
 
 
-if args.fp15 == True:
+if args.fp16 == True:
     model.half()  # convert to FP16 on GPU
     for layer in model.modules():
         if isinstance(layer, nn.BatchNorm1d):
