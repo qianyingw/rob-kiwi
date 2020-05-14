d226faf20dcf2dd6eb6e020892014a1c54907600
Qianying Wang <w.qianying@outlook.com>
2020-05-14 17:47:14 +0100

    conversion for bert/albert

--- bert/tf_to_torch.py
+++ bert/tf_to_torch.py
@@ -20,31 +20,61 @@
 #    @author: qwang
 """
 
-
 import os
 import torch
 from transformers import BertConfig, BertForPreTraining, load_tf_weights_in_bert
+from transformers import AlbertConfig, AlbertForPreTraining, load_tf_weights_in_albert
 
 
-# Download pre-trained weights of BioBERT from https://github.com/dmis-lab/biobert
-BIO_PATH = "/media/mynewdrive/rob/data/biobert/biobert_v1.1_pubmed"
 
-                      
 def convert_tf_checkpoint_to_pytorch(tf_checkpoint_path, bert_config_file, pytorch_dump_path):
-    # Initialise PyTorch model
-    config = BertConfig.from_json_file(bert_config_file)
+    
+    config = BertConfig.from_json_file(bert_config_file)  # Initialise PyTorch model
     print("Building PyTorch model from configuration: {}".format(str(config)))
     model = BertForPreTraining(config)
 
-    # Load weights from tf checkpoint
-    load_tf_weights_in_bert(model, config, tf_checkpoint_path)
+    load_tf_weights_in_bert(model, config, tf_checkpoint_path)  # Load weights from tf checkpoint
 
-    # Save pytorch-model
     print("Save PyTorch model to {}".format(pytorch_dump_path))
-    torch.save(model.state_dict(), pytorch_dump_path)
+    torch.save(model.state_dict(), pytorch_dump_path)  # Save pytorch-model
     
     
-bert_config_file = os.path.join(BIO_PATH, "bert_config.json")
-tf_checkpoint_path = os.path.join(BIO_PATH, "model.ckpt-1000000")
-pytorch_dump_path = os.path.join(BIO_PATH, "pytorch_model.bin")
+    
+def convert_tf_checkpoint_to_pytorch_albert(tf_checkpoint_path, albert_config_file, pytorch_dump_path):
+    
+    config = AlbertConfig.from_json_file(albert_config_file)  # Initialise PyTorch model
+    print("Building PyTorch model from configuration: {}".format(str(config)))
+    model = AlbertForPreTraining(config)
+
+    load_tf_weights_in_albert(model, config, tf_checkpoint_path)  # Load weights from tf checkpoint
+
+    print("Save PyTorch model to {}".format(pytorch_dump_path))
+    torch.save(model.state_dict(), pytorch_dump_path)  # Save pytorch-model
+
+#%% BERT-Medium
+# Downloaded from https://storage.googleapis.com/bert_models/2020_02_20/uncased_L-8_H-512_A-8.zip
+wgt_dir = "/media/mynewdrive/rob/data/pre_wgts/source/uncased_L-8_H-512_A-8"
+bert_config_file = os.path.join(wgt_dir, "bert_config.json")
+tf_checkpoint_path = os.path.join(wgt_dir, "bert_model.ckpt")
+pytorch_dump_path = os.path.join(wgt_dir, "pytorch_model.bin")
+
+convert_tf_checkpoint_to_pytorch(tf_checkpoint_path, bert_config_file, pytorch_dump_path)
+
+#%% ALBERT-base  
+# Download from https://storage.googleapis.com/albert_models/albert_base_v2.tar.gz
+wgt_dir = "/media/mynewdrive/rob/data/pre_wgts/source/albert_base_v2"
+albert_config_file = os.path.join(wgt_dir, "albert_config.json")
+tf_checkpoint_path = os.path.join(wgt_dir, "model.ckpt-best")
+pytorch_dump_path = os.path.join(wgt_dir, "pytorch_model.bin")
+
+convert_tf_checkpoint_to_pytorch_albert(tf_checkpoint_path, albert_config_file, pytorch_dump_path)
+
+#%% BioBERT    
+# Download from https://github.com/dmis-lab/biobert
+wgt_dir = "/media/mynewdrive/rob/data/pre_wgts/source/biobert_v1.1_pubmed"
+
+bert_config_file = os.path.join(wgt_dir, "bert_config.json")
+tf_checkpoint_path = os.path.join(wgt_dir, "model.ckpt-1000000")
+pytorch_dump_path = os.path.join(wgt_dir, "pytorch_model.bin")
+
 convert_tf_checkpoint_to_pytorch(tf_checkpoint_path, bert_config_file, pytorch_dump_path)
