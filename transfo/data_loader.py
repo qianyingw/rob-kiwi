fb874a53e4d9c1140792c6765c79cd8b4d0f95be
Qianying Wang <w.qianying@outlook.com>
2020-05-21 18:10:39 +0100

    xlnet input format

--- transfo/data_loader.py
+++ transfo/data_loader.py
@@ -15,6 +15,7 @@ import torch.nn as nn
 from torch.utils.data import Dataset
 import sklearn
 
+import transformers
    
 #%%
 class DocDataset(Dataset):
@@ -71,47 +72,84 @@ class DocDataset(Dataset):
         else:
             sent_ls = sent_ls[n_head : -n_tail]
         
-        # Split sentence lists to tokens
-        tokens = []
-        for l in sent_ls:
-            tokens = tokens + l[0].split(" ")
-            
-        # Convert tokens back to plain text
-        text = " ".join(tokens)
-        # Convert text to bert tokens by WordPiece
-        tokens = self.tokenizer.tokenize(text)
-        
-        # Split tokens into chunks
-        n_chunks = len(tokens) // (self.max_chunk_len - 2)
-        if len(tokens) % (self.max_chunk_len - 2) != 0:
-            n_chunks += 1           
-        
-        # Limit number of chunks
-        if n_chunks > self.max_n_chunk:
-            tokens = tokens[:self.max_chunk_len*self.max_n_chunk]
-            n_chunks = self.max_n_chunk  
-        assert n_chunks <= self.max_n_chunk, "The document is too large. Try to increase cut_head/tail_ratio."
-        
-        # Document tensor
-        doc = torch.zeros((n_chunks, 3, self.max_chunk_len), dtype=torch.long)
-        for i in range(n_chunks):
-            chunk_tokens = tokens[(self.max_chunk_len-2) * i : (self.max_chunk_len-2) * (i+1)]
-            chunk_tokens.insert(0, "[CLS]")
-            chunk_tokens.append("[SEP]")              
-            chunk_tokens_ids = self.tokenizer.convert_tokens_to_ids(chunk_tokens)
-                  
-            attn_masks = [1] * len(chunk_tokens_ids)         
-            # Pad the last chunk
-            while len(chunk_tokens_ids) < self.max_chunk_len:
-                chunk_tokens_ids.append(0)
-                attn_masks.append(0)
+        ###### BERT input tokenization and formatting ######
+        if type(self.tokenizer) == transformers.tokenization_bert.BertTokenizer:
+            # Split sentence lists to tokens
+            tokens = []
+            for l in sent_ls:
+                tokens = tokens + l[0].split(" ")
                 
-            token_type_ids = [0] * self.max_chunk_len       
-            assert len(chunk_tokens_ids) == self.max_chunk_len and len(attn_masks) == self.max_chunk_len
+            # Convert tokens back to plain text
+            text = " ".join(tokens)
+            # Convert text to tokens by WordPiece
+            tokens = self.tokenizer.tokenize(text)
+            
+            # Split tokens into chunks
+            n_chunks = len(tokens) // (self.max_chunk_len - 2)
+            if len(tokens) % (self.max_chunk_len - 2) != 0:
+                n_chunks += 1           
+            
+            # Limit number of chunks
+            if n_chunks > self.max_n_chunk:
+                tokens = tokens[:self.max_chunk_len*self.max_n_chunk]
+                n_chunks = self.max_n_chunk  
+            assert n_chunks <= self.max_n_chunk, "The document is too large. Try to increase cut_head/tail_ratio."
+            
+            # Document tensor
+            doc = torch.zeros((n_chunks, 3, self.max_chunk_len), dtype=torch.long)
+            for i in range(n_chunks):
+                chunk_tokens = tokens[(self.max_chunk_len-2) * i : (self.max_chunk_len-2) * (i+1)]
+                chunk_tokens.insert(0, "[CLS]")
+                chunk_tokens.append("[SEP]")              
+                chunk_tokens_ids = self.tokenizer.convert_tokens_to_ids(chunk_tokens)
+                      
+                attn_masks = [1] * len(chunk_tokens_ids)         
+                # Pad the last chunk
+                while len(chunk_tokens_ids) < self.max_chunk_len:
+                    chunk_tokens_ids.append(0)
+                    attn_masks.append(0)
+                    
+                token_type_ids = [0] * self.max_chunk_len       
+                assert len(chunk_tokens_ids) == self.max_chunk_len and len(attn_masks) == self.max_chunk_len
+                             
+                doc[i] = torch.cat((torch.LongTensor(chunk_tokens_ids).unsqueeze(0),
+                                    torch.LongTensor(attn_masks).unsqueeze(0),
+                                    torch.LongTensor(token_type_ids).unsqueeze(0)), dim=0)
+        
+        ###### XLNet input tokenization and formatting ######  
+        elif type(self.tokenizer) == transformers.tokenization_xlnet.XLNetTokenizer:
+            # Split sentence lists to tokens
+            # An XLNet sequence has the following format: A <sep> B <sep> <cls>
+            tokens = []
+            for l in sent_ls:
+                tokens = tokens + l[0].split(" ")
+                tokens.append("<sep>")
+            
+            # Convert tokens back to plain text
+            text = " ".join(tokens)
+            # Convert text to tokens by SentencePiece
+            tokens = self.tokenizer.tokenize(text)
+            
+            # Limit number of tokens
+            # 'max_n_chunk' to BERT is 'max_n_token' to xlnet (just for args simplification)
+            n_tokens = len(tokens)  
+                           
+            if n_tokens > self.max_n_chunk and self.max_n_chunk > 20:
+                tokens = tokens[:self.max_n_chunk] 
+                n_tokens = self.max_n_chunk
+            assert n_tokens <= self.max_n_chunk, "The document is too large. Try to increase cut_head/tail_ratio."      
+            
+            tokens.append("<cls>")
+            
+            # Document tensor
+            doc = torch.zeros((n_tokens, 2), dtype=torch.long)
+            token_ids = self.tokenizer.convert_tokens_to_ids(tokens)
+            
+            attn_masks = [1] * len(token_ids) 
+            # token_type_ids = [0] * len(token_ids)
                          
-            doc[i] = torch.cat((torch.LongTensor(chunk_tokens_ids).unsqueeze(0),
-                                torch.LongTensor(attn_masks).unsqueeze(0),
-                                torch.LongTensor(token_type_ids).unsqueeze(0)), dim=0)
+            doc = torch.cat((torch.LongTensor(token_ids).unsqueeze(0),
+                             torch.LongTensor(attn_masks).unsqueeze(0)), dim=0)
         
         # Label tensor
         label = self.info_df.loc[idx, self.rob_item]
@@ -130,7 +168,9 @@ class PadDoc:
     def __call__(self, batch):
         # Element in batch: (doc, label)
         # Sort batch by num_chunks in descending order. x[0] => doc, x[1] -> label
-        # x[0].shape = [num_chunks, 3, max_chunk_len]
+        # x[0].shape
+        #       BERT: [num_chunks, 3, max_chunk_len]
+        #       XLNet: [n_tokens, 2]
         sorted_batch = sorted(batch, key=lambda x: x[0].shape[0], reverse=True)  # 
         
         # Pad doc within batch       		
@@ -143,6 +183,8 @@ class PadDoc:
         # Store length of each doc for unpad them later
         lens = torch.LongTensor([len(x) for x in docs])  
         return docs_padded, labels, lens
+
+
       
 #%% Instance   
 #data_set = DocDataset(info_file = '/media/mynewdrive/rob/data/rob_info_a.pkl',
